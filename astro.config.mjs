434906151b10963b09457568296f4a745297b40c
humid1ch <dxyt2002@163.com>
2025-10-31 11:32:08 +0800

    fix vercel pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,10 @@ import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeComponents from "rehype-components";/* Render the custom directive content */
+import rehypeComponents from "rehype-components"; /* Render the custom directive content */
 import rehypeKatex from "rehype-katex";
 import rehypeSlug from "rehype-slug";
-import remarkDirective from "remark-directive";/* Handle directives */
+import remarkDirective from "remark-directive"; /* Handle directives */
 import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
 import remarkMath from "remark-math";
 import remarkSectionize from "remark-sectionize";
@@ -24,9 +24,9 @@ import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-copy-button.js";
 
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import vercel from "@astrojs/vercel";
 
@@ -35,172 +35,176 @@ const __dirname = path.dirname(__filename);
 
 // 读取并确保格式正确
 const rawGrammar = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'src/syntaxes/Cangjie.tmLanguage.json'), 'utf-8')
+  fs.readFileSync(
+    path.join(__dirname, "src/syntaxes/Cangjie.tmLanguage.json"),
+    "utf-8"
+  )
 );
 
 // Shiki 需要的格式
 const cangjieGrammar = {
   ...rawGrammar,
   // 确保这些字段存在
-  name: rawGrammar.name || 'Cangjie',
-  scopeName: rawGrammar.scopeName || 'source.cj',
+  name: rawGrammar.name || "Cangjie",
+  scopeName: rawGrammar.scopeName || "source.cj",
 };
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://blog.humid1ch.cn/",
   base: "/",
   trailingSlash: "always",
 
   integrations: [
-      tailwind({
-          nesting: true,
-      }),
-      swup({
-          theme: false,
-          animationClass: "transition-swup-", // see https://swup.js.org/options/#animationselector
-          // the default value `transition-` cause transition delay
-          // when the Tailwind class `transition-all` is used
-          containers: ["main", "#toc"],
-          smoothScrolling: true,
-          cache: true,
-          preload: true,
-          accessibility: true,
-          updateHead: true,
-          updateBodyClass: false,
-          globalInstance: true,
-      }),
-      icon({
-          include: {
-              "preprocess: vitePreprocess(),": ["*"],
-              "fa6-brands": ["*"],
-              "fa6-regular": ["*"],
-              "fa6-solid": ["*"],
+    tailwind({
+      nesting: true,
+    }),
+    swup({
+      theme: false,
+      animationClass: "transition-swup-", // see https://swup.js.org/options/#animationselector
+      // the default value `transition-` cause transition delay
+      // when the Tailwind class `transition-all` is used
+      containers: ["main", "#toc"],
+      smoothScrolling: true,
+      cache: true,
+      preload: true,
+      accessibility: true,
+      updateHead: true,
+      updateBodyClass: false,
+      globalInstance: true,
+    }),
+    icon({
+      include: {
+        "preprocess: vitePreprocess(),": ["*"],
+        "fa6-brands": ["*"],
+        "fa6-regular": ["*"],
+        "fa6-solid": ["*"],
+      },
+    }),
+    expressiveCode({
+      themes: [expressiveCodeConfig.theme, expressiveCodeConfig.theme],
+      plugins: [
+        pluginCollapsibleSections(),
+        pluginLineNumbers(),
+        pluginLanguageBadge(),
+        pluginCustomCopyButton(),
+      ],
+      defaultProps: {
+        wrap: true,
+        overridesByLang: {
+          shellsession: {
+            showLineNumbers: false,
           },
-      }),
-      expressiveCode({
-          themes: [expressiveCodeConfig.theme, expressiveCodeConfig.theme],
-          plugins: [
-              pluginCollapsibleSections(),
-              pluginLineNumbers(),
-              pluginLanguageBadge(),
-              pluginCustomCopyButton()
-          ],
-          defaultProps: {
-              wrap: true,
-              overridesByLang: {
-                  'shellsession': {
-                      showLineNumbers: false,
-                  },
-              },
-          },
-          styleOverrides: {
-              codeBackground: "var(--codeblock-bg)",
-              borderRadius: "0.75rem",
-              borderColor: "none",
-              codeFontSize: "0.925rem",
-              codeFontFamily: "'Maple Mono Normal Medium', 'HarmonyOS Sans SC Medium'",
-              codeLineHeight: "1.41414rem",
-              frames: {
-                  editorBackground: "var(--codeblock-bg)",
-                  terminalBackground: "var(--codeblock-bg)",
-                  terminalTitlebarBackground: "var(--codeblock-topbar-bg)",
-                  editorTabBarBackground: "var(--codeblock-topbar-bg)",
-                  editorActiveTabBackground: "none",
-                  editorActiveTabIndicatorBottomColor: "var(--primary)",
-                  editorActiveTabIndicatorTopColor: "none",
-                  editorTabBarBorderBottomColor: "var(--codeblock-topbar-bg)",
-                  terminalTitlebarBorderBottomColor: "none"
-              },
-              textMarkers: {
-                  delHue: 0,
-                  insHue: 180,
-                  markHue: 250
-              }
-          },
-          frames: {
-              showCopyToClipboardButton: false,
-          },
-          shiki: {
-              // You can pass additional plugin options here,
-              // e.g. to load custom language grammars:
-              langs: [
-                  cangjieGrammar,  // 直接传入语法对象，不需要包装
-              ],
-          },
-      }),
-      svelte(),
-      sitemap(),
-	],
+        },
+      },
+      styleOverrides: {
+        codeBackground: "var(--codeblock-bg)",
+        borderRadius: "0.75rem",
+        borderColor: "none",
+        codeFontSize: "0.925rem",
+        codeFontFamily:
+          "'Maple Mono Normal Medium', 'HarmonyOS Sans SC Medium'",
+        codeLineHeight: "1.41414rem",
+        frames: {
+          editorBackground: "var(--codeblock-bg)",
+          terminalBackground: "var(--codeblock-bg)",
+          terminalTitlebarBackground: "var(--codeblock-topbar-bg)",
+          editorTabBarBackground: "var(--codeblock-topbar-bg)",
+          editorActiveTabBackground: "none",
+          editorActiveTabIndicatorBottomColor: "var(--primary)",
+          editorActiveTabIndicatorTopColor: "none",
+          editorTabBarBorderBottomColor: "var(--codeblock-topbar-bg)",
+          terminalTitlebarBorderBottomColor: "none",
+        },
+        textMarkers: {
+          delHue: 0,
+          insHue: 180,
+          markHue: 250,
+        },
+      },
+      frames: {
+        showCopyToClipboardButton: false,
+      },
+      shiki: {
+        // You can pass additional plugin options here,
+        // e.g. to load custom language grammars:
+        langs: [
+          cangjieGrammar, // 直接传入语法对象，不需要包装
+        ],
+      },
+    }),
+    svelte(),
+    sitemap(),
+  ],
 
   markdown: {
-      remarkPlugins: [
-          remarkMath,
-          remarkReadingTime,
-          remarkExcerpt,
-          remarkGithubAdmonitionsToDirectives,
-          remarkDirective,
-          remarkSectionize,
-          parseDirectiveNode,
+    remarkPlugins: [
+      remarkMath,
+      remarkReadingTime,
+      remarkExcerpt,
+      remarkGithubAdmonitionsToDirectives,
+      remarkDirective,
+      remarkSectionize,
+      parseDirectiveNode,
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeSlug,
+      [
+        rehypeComponents,
+        {
+          components: {
+            github: GithubCardComponent,
+            note: (x, y) => AdmonitionComponent(x, y, "note"),
+            tip: (x, y) => AdmonitionComponent(x, y, "tip"),
+            important: (x, y) => AdmonitionComponent(x, y, "important"),
+            caution: (x, y) => AdmonitionComponent(x, y, "caution"),
+            warning: (x, y) => AdmonitionComponent(x, y, "warning"),
+          },
+        },
       ],
-      rehypePlugins: [
-          rehypeKatex,
-          rehypeSlug,
-          [
-              rehypeComponents,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          properties: {
+            className: ["anchor"],
+          },
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: {
+              className: ["anchor-icon"],
+              "data-pagefind-ignore": true,
+            },
+            children: [
               {
-                  components: {
-                      github: GithubCardComponent,
-                      note: (x, y) => AdmonitionComponent(x, y, "note"),
-                      tip: (x, y) => AdmonitionComponent(x, y, "tip"),
-                      important: (x, y) => AdmonitionComponent(x, y, "important"),
-                      caution: (x, y) => AdmonitionComponent(x, y, "caution"),
-                      warning: (x, y) => AdmonitionComponent(x, y, "warning"),
-                  },
+                type: "text",
+                value: "#",
               },
-          ],
-          [
-              rehypeAutolinkHeadings,
-              {
-                  behavior: "append",
-                  properties: {
-                      className: ["anchor"],
-                  },
-                  content: {
-                      type: "element",
-                      tagName: "span",
-                      properties: {
-                          className: ["anchor-icon"],
-                          "data-pagefind-ignore": true,
-                      },
-                      children: [
-                          {
-                              type: "text",
-                              value: "#",
-                          },
-                      ],
-                  },
-              },
-          ],
+            ],
+          },
+        },
       ],
-	},
+    ],
+  },
 
   vite: {
-      build: {
-          rollupOptions: {
-              onwarn(warning, warn) {
-                  // temporarily suppress this warning
-                  if (
-                      warning.message.includes("is dynamically imported by") &&
-                      warning.message.includes("but also statically imported by")
-                  ) {
-                      return;
-                  }
-                  warn(warning);
-              },
-          },
+    build: {
+      rollupOptions: {
+        onwarn(warning, warn) {
+          // temporarily suppress this warning
+          if (
+            warning.message.includes("is dynamically imported by") &&
+            warning.message.includes("but also statically imported by")
+          ) {
+            return;
+          }
+          warn(warning);
+        },
       },
-	},
+    },
+  },
 
   adapter: vercel(),
 });
