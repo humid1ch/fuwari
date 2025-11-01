5fc90dc309145d159c1c388baf2b9dcd9d75b8dc
humid1ch <dxyt2002@163.com>
2025-11-01 17:18:46 +0800

    fix article

--- src/config.ts
+++ src/config.ts
@@ -18,8 +18,8 @@ export const siteConfig: SiteConfig = {
   },
   banner: {
     enable: true,
-    src: "assets/images/banner1.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: "top", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    src: "assets/images/banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
       enable: false, // Display the credit text of the banner image
       text: "", // Credit text to be displayed
@@ -28,15 +28,15 @@ export const siteConfig: SiteConfig = {
   },
   toc: {
     enable: true, // Display the table of contents on the right side of the post
-    depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+    depth: 3, // Maximum heading depth to show in the table, from 1 to 3
   },
   favicon: [
     // Leave this array empty to use the default favicon
     // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+    //   src: "/public/favicon/favicon-light-32.png", // Path of the favicon, relative to the /public directory
+    //   theme: "light", // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+    //   sizes: "32x32", // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    // },
   ],
 };
 
